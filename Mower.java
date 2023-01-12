c0fdc09415bfb02f0bf40067528e16c99ab6d5ce
M'Bareck Moahmed Said <said.mbareck@adias.fr>
2023-01-12 12:50:01 +0000

    First working code

---
+++ Mower.java
@@ -0,0 +1,109 @@
+public class Mower {
+    Position cp; // Current position
+
+
+    public  Position move(String path){
+
+        for (char O: path.toCharArray()) {
+            
+            switch(O + "") {
+                case "D":
+                  turnD();
+                  break;
+                case "G":
+                  turnG();
+                  break;
+                case "A":
+                  step(cp.getO());
+                  break;
+                default:
+                    break;
+              }
+              
+        }
+          
+        return this.cp;// code block
+    }
+
+
+    public void turnD(){
+        switch (cp.getO()) {// code block
+            case "N":
+                cp.setO("E");
+                break;
+            case "E":// code block
+                cp.setO("S");
+                break;
+            case "S":
+                cp.setO("W");
+                break;
+            case "W":
+                cp.setO("N");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void turnG(){
+        switch (cp.getO()) {
+            case "N":
+                cp.setO("W");
+                break;
+            case "E":
+                cp.setO("N");
+                break;
+            case "S":
+                cp.setO("E");
+                break;
+            case "W":
+                cp.setO("S");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public Position step(String O){//O="A"
+        switch (O) {
+            case "N":
+                cp.setY(cp.getY()+1);
+                break;
+            case "E":
+                cp.setX(cp.getX()+1);
+                break;
+            case "S":
+                cp.setY(cp.getY()-1);
+                break;
+            case "W":
+                cp.setX(cp.getX()-1);
+                break;
+            default:
+                break;
+        }
+        return this.cp;
+    }
+
+
+
+    public Mower() {
+    }
+
+    public Mower(Position cp) {
+        this.cp = cp;
+    }
+
+    public Position getCp() {
+        return this.cp;
+    }
+
+    public void setCp(Position cp) {
+        this.cp = cp;
+    }
+
+    public Mower cp(Position cp) {
+        setCp(cp);
+        return this;
+    }
+}
---
+++ Position.java
@@ -0,0 +1,59 @@
+public class Position {
+    int x;
+    int y;
+    String o; // (N,E,W,S)
+
+    public Position() {
+    }
+
+    public Position(int x, int y, String o) {
+        this.x = x;
+        this.y = y;
+        this.o = o;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getO() {
+        return this.o;
+    }
+
+    public void setO(String o) {
+        this.o = o;
+    }
+
+    public Position x(int x) {
+        setX(x);
+        return this;
+    }
+
+    public Position y(int y) {
+        setY(y);
+        return this;
+    }
+
+    public Position o(String o) {
+        setO(o);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Position [x=" + x + ", y=" + y + ", o=" + o + "]";
+    }
+    
+}
